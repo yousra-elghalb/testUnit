10e91dbc66109abb7814ed0b13582f2e303d0ac9
yousra-elghalb <46228092+yousra-elghalb@users.noreply.github.com>
2020-03-10 16:28:21 +0100

    adds basic test for Book

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::post('/books','BooksController@store');
+Route::patch('/books/{book}','BooksController@update');

a63814bfc58bde89b7eba1b6733a10deab23d247
yousra-elghalb <46228092+yousra-elghalb@users.noreply.github.com>
2020-03-10 16:22:23 +0100

    adds the Book resource

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Book;
+
+class BooksController extends Controller
+{
+    public function store(){
+
+        Book::create($this->validateRequest());
+
+    }
+
+    public function update(Book $book){
+
+        $book->update($this ->validateRequest());
+    }
+
+    /**
+     * @return mixed
+     */
+
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required',
+        ]);
+
+    }
+
+
+}

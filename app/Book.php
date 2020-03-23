7e8585ba0263e2c4bc839016ee7715c9b6307019
yousra-elghalb <46228092+yousra-elghalb@users.noreply.github.com>
2020-03-23 17:22:22 +0100

    adds new path to books resource

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $guarded =[];
+
+    //si on veut personaliser le path, on peut le faire ici au lieu de changer ds tte les methods
+    public function path()
+    {
+        return '/books/' . $this->id ;
+    }
 }
